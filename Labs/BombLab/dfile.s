aa08004525ecb1e7ee0f8dd8d6befc64ed7aaecd
lhz90529 <36825650+lhz90529@users.noreply.github.com>
2018-05-01 17:41:24 -0400

    Add files via upload

--- Labs/BombLab/dfile.s
+++ Labs/BombLab/dfile.s
@@ -515,43 +515,45 @@ Disassembly of section .text:
   4010fa:	55                   	push   %rbp
   4010fb:	53                   	push   %rbx
   4010fc:	48 83 ec 50          	sub    $0x50,%rsp
-  401100:	49 89 e5             	mov    %rsp,%r13
+  401100:	49 89 e5             	mov    %rsp,%r13    
   401103:	48 89 e6             	mov    %rsp,%rsi
   401106:	e8 51 03 00 00       	callq  40145c <read_six_numbers>
-  40110b:	49 89 e6             	mov    %rsp,%r14
-  40110e:	41 bc 00 00 00 00    	mov    $0x0,%r12d
-  401114:	4c 89 ed             	mov    %r13,%rbp
-  401117:	41 8b 45 00          	mov    0x0(%r13),%eax
-  40111b:	83 e8 01             	sub    $0x1,%eax
-  40111e:	83 f8 05             	cmp    $0x5,%eax
-  401121:	76 05                	jbe    401128 <phase_6+0x34>
+  40110b:	49 89 e6             	mov    %rsp,%r14    # rsp and r14 point to arg1
+  40110e:	41 bc 00 00 00 00    	mov    $0x0,%r12d   # %r12 = 0
+  401114:	4c 89 ed             	mov    %r13,%rbp    # %r13 and rbp point to arg1
+  401117:	41 8b 45 00          	mov    0x0(%r13),%eax # now %rax stores the value of arg1
+  40111b:	83 e8 01             	sub    $0x1,%eax      # %rax = args1 - 1
+  40111e:	83 f8 05             	cmp    $0x5,%eax      # arg1 - 1 - 5 ? 0
+  401121:	76 05                	jbe    401128 <phase_6+0x34>  # skip explod if 1 <= arg1 <= 6
   401123:	e8 12 03 00 00       	callq  40143a <explode_bomb>
-  401128:	41 83 c4 01          	add    $0x1,%r12d
-  40112c:	41 83 fc 06          	cmp    $0x6,%r12d
-  401130:	74 21                	je     401153 <phase_6+0x5f>
-  401132:	44 89 e3             	mov    %r12d,%ebx
-  401135:	48 63 c3             	movslq %ebx,%rax
-  401138:	8b 04 84             	mov    (%rsp,%rax,4),%eax
-  40113b:	39 45 00             	cmp    %eax,0x0(%rbp)
-  40113e:	75 05                	jne    401145 <phase_6+0x51>
+  401128:	41 83 c4 01          	add    $0x1,%r12d     # %r12 = 1
+  40112c:	41 83 fc 06          	cmp    $0x6,%r12d     # %r12 is definitely not equal to 6
+  401130:	74 21                	je     401153 <phase_6+0x5f>  # never executed
+  401132:	44 89 e3             	mov    %r12d,%ebx     # %rbx = 1
+  401135:	48 63 c3             	movslq %ebx,%rax      # %rax also = 1
+  401138:	8b 04 84             	mov    (%rsp,%rax,4),%eax # move arg2 to %eax, %eax stores arg2
+  40113b:	39 45 00             	cmp    %eax,0x0(%rbp)     # arg1 - arg2 ? 0
+  40113e:	75 05                	jne    401145 <phase_6+0x51>  # skip explode if arg2 is not equal to arg1
   401140:	e8 f5 02 00 00       	callq  40143a <explode_bomb>
-  401145:	83 c3 01             	add    $0x1,%ebx
-  401148:	83 fb 05             	cmp    $0x5,%ebx
-  40114b:	7e e8                	jle    401135 <phase_6+0x41>
-  40114d:	49 83 c5 04          	add    $0x4,%r13
+  401145:	83 c3 01             	add    $0x1,%ebx          # %ebx = 2 (%ebx++)
+  401148:	83 fb 05             	cmp    $0x5,%ebx          # while (%ebx <= 5)
+  40114b:	7e e8                	jle    401135 <phase_6+0x41>  # go to the begining of loop
+  40114d:	49 83 c5 04          	add    $0x4,%r13          # %r13 is a pointer point to next args
   401151:	eb c1                	jmp    401114 <phase_6+0x20>
-  401153:	48 8d 74 24 18       	lea    0x18(%rsp),%rsi
-  401158:	4c 89 f0             	mov    %r14,%rax
-  40115b:	b9 07 00 00 00       	mov    $0x7,%ecx
-  401160:	89 ca                	mov    %ecx,%edx
-  401162:	2b 10                	sub    (%rax),%edx
-  401164:	89 10                	mov    %edx,(%rax)
-  401166:	48 83 c0 04          	add    $0x4,%rax
-  40116a:	48 39 f0             	cmp    %rsi,%rax
+  # #################################################################
+  401153:	48 8d 74 24 18       	lea    0x18(%rsp),%rsi    # %rsi point to 18 bytes away from rsp (address of input array)
+  401158:	4c 89 f0             	mov    %r14,%rax          # %r14 = %rax = address of arg1
+  40115b:	b9 07 00 00 00       	mov    $0x7,%ecx          # %rcx = 7
+  401160:	89 ca                	mov    %ecx,%edx          # %rdx = 7
+  401162:	2b 10                	sub    (%rax),%edx        # rdx = 7 - args1 
+  401164:	89 10                	mov    %edx,(%rax)        # args1 = rdx i.e. args1 = 7 - args1
+  401166:	48 83 c0 04          	add    $0x4,%rax          # %rax = args2
+  40116a:	48 39 f0             	cmp    %rsi,%rax          # 
   40116d:	75 f1                	jne    401160 <phase_6+0x6c>
-  40116f:	be 00 00 00 00       	mov    $0x0,%esi
-  401174:	eb 21                	jmp    401197 <phase_6+0xa3>
-  401176:	48 8b 52 08          	mov    0x8(%rdx),%rdx
+  # #################################################################
+  40116f:	be 00 00 00 00       	mov    $0x0,%esi          # set %rsi = 0
+  401174:	eb 21                	jmp    401197 <phase_6+0xa3>  # go to 401197 
+  401176:	48 8b 52 08          	mov    0x8(%rdx),%rdx         # move !!!one!!! byte from address "rdx + 8"
   40117a:	83 c0 01             	add    $0x1,%eax
   40117d:	39 c8                	cmp    %ecx,%eax
   40117f:	75 f5                	jne    401176 <phase_6+0x82>
@@ -559,14 +561,15 @@ Disassembly of section .text:
   401183:	ba d0 32 60 00       	mov    $0x6032d0,%edx
   401188:	48 89 54 74 20       	mov    %rdx,0x20(%rsp,%rsi,2)
   40118d:	48 83 c6 04          	add    $0x4,%rsi
-  401191:	48 83 fe 18          	cmp    $0x18,%rsi
+  401191:	48 83 fe 18          	cmp    $0x18,%rsi           # check if %rsi = 24
   401195:	74 14                	je     4011ab <phase_6+0xb7>
-  401197:	8b 0c 34             	mov    (%rsp,%rsi,1),%ecx
-  40119a:	83 f9 01             	cmp    $0x1,%ecx
-  40119d:	7e e4                	jle    401183 <phase_6+0x8f>
-  40119f:	b8 01 00 00 00       	mov    $0x1,%eax
-  4011a4:	ba d0 32 60 00       	mov    $0x6032d0,%edx
-  4011a9:	eb cb                	jmp    401176 <phase_6+0x82>
+  401197:	8b 0c 34             	mov    (%rsp,%rsi,1),%ecx   # %ecx = args1 
+  40119a:	83 f9 01             	cmp    $0x1,%ecx            #
+  40119d:	7e e4                	jle    401183 <phase_6+0x8f>  # if the current argument <= 1, go back to 401183
+  40119f:	b8 01 00 00 00       	mov    $0x1,%eax              # else %eax = 1 (constant)
+  4011a4:	ba d0 32 60 00       	mov    $0x6032d0,%edx         # %edx  = 0x6032d0
+  4011a9:	eb cb                	jmp    401176 <phase_6+0x82>  #
+  # #########################################################################
   4011ab:	48 8b 5c 24 20       	mov    0x20(%rsp),%rbx
   4011b0:	48 8d 44 24 28       	lea    0x28(%rsp),%rax
   4011b5:	48 8d 74 24 50       	lea    0x50(%rsp),%rsi

